7426ae94bbccbcae1ae0b2514cf0df6ba77e5e76
eddieg00 <119461197+eddieg00@users.noreply.github.com>
2023-04-20 13:29:17 -0400

    fixed minor typo in get by id route for tags

--- fantastic-umbrella-main/Develop/routes/api/product-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/product-routes.js
@@ -4,11 +4,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findAll({
+    const productData = await Product.findAll({
       include: [{model:Category}, {model: Tag}]
     })
     res.status(200).json(productData)
@@ -18,11 +18,11 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = Product.findByPk(req.params.id, {
+    const productData = await Product.findByPk(req.params.id, {
       include: [{model:Category}, {model: Tag}]
     })
 
@@ -110,10 +110,10 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = Product.destroy({
+    const productData = await Product.destroy({
       where: {
         id: req.params.id
       }
--- fantastic-umbrella-main/Develop/routes/api/tag-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/tag-routes.js
@@ -3,11 +3,11 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = Tag.findAll({
+    const tagData = await Tag.findAll({
       include: [{model: Product}]
     })
     res.status(200).json(tagData)
@@ -16,12 +16,12 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = Tag.findByPk(req.params.id, {
-      include: [ {model: Tag}]
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [ {model: Product}]
     })
 
     if(!tagData) {
@@ -34,20 +34,20 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
   try {
-    const tagData = Tag.create(req.body)
+    const tagData = await Tag.create(req.body)
     res.status(200).json(tagData)
   } catch(err) {
     res.status(500).json(err)
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = Tag.update({
+    const tagData = await Tag.update({
       tag_name: req.body.tag_name
     }, {
       where: {
@@ -65,10 +65,10 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete on tag by its `id` value
   try {
-    const tagData = Tag.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id
       }
